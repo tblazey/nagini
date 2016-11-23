a40dd789edb6e7da8b9f9300a9cebf9fce13f084
Tyler Blazey <blazey@wustl.edu>
2016-11-23 07:53:08 -0600

    First commit PVC script

--- pvcCalc.py
+++ pvcCalc.py
@@ -13,6 +13,7 @@ args = arg_parse.parse_args()
 #Import libraries
 import numpy as np, nibabel as nib, scipy.ndimage.filters as filt
 import scipy.optimize as opt, nagini, sys
+from tqdm import tqdm
 
 #Load headers
 pet = nagini.loadHeader(args.pet[0])
@@ -30,7 +31,7 @@ if args.mask is not None:
 	mask = nagini.loadHeader(args.mask[0])
 
 	#Check to make sure dimensions mask
-	if mask.shape != pet.shape[0:3]:
+	if mask.shape[0:3] != pet.shape[0:3]:
 		print 'ERROR: Mask image does not have same dimensions other data...'
 		sys.exit()
 
@@ -52,12 +53,12 @@ segData = seg.get_data()
 if len(segData.shape) == 4:
 	segData = segData[:,:,:,0]
 
-#Reshape PET data
+#Reshape PET data if necessary
 if len(petData.shape) == 4:
 	petData = nagini.reshape4d(petData)
 	nPet = petData.shape[1]
 else:
-	petData = petData.flatten()
+	petData = petData.reshape((pet.shape[0]*pet.shape[1]*pet.shape[2],1))
 	nPet = 1
 
 #Get ROI list and number of ROIs
@@ -66,45 +67,51 @@ if args.noZero == 1:
 	roiList = roiList[roiList!=0]
 nRoi = roiList.shape[0]
 
-#Get regression matrix
-xMat = np.zeros((seg.shape[0],seg.shape[1],seg.shape[2],nRoi))
-for roiIdx in range(nRoi):
-	print roiIdx
-
-	#Make ROI image
-	roiData = np.zeros_like(segData)
-	roiData[segData==roiList[roiIdx]] = 1
-
-	#Smooth the ROI image
-	xMat[:,:,:,roiIdx] =  filt.gaussian_filter(roiData,3.397)
-
-#Reshape design matrix
-xMat = nagini.reshape4d(xMat)
-
 #Make weight matrices
-wMatrix = np.zeros((nRoi,nRoi))
-tMatrix = np.zeros((nRoi,nPet))
+wMatrix = np.zeros((nRoi,nRoi),dtype=np.float64)
+tMatrix = np.zeros((nRoi,nPet),dtype=np.float64)
 
 #Get weighted values
-for roiIdx in range(nRoi):
-	print roiIdx
+for iIdx in range(nRoi):
+	print iIdx
 
-	#Calculate weighting matrix
-	wMatrix[:,roiIdx] = np.dot(xMat[:,roiIdx],xMat)
+	#Make i ROI
+	iRoi = np.zeros((seg.shape[0],seg.shape[1],seg.shape[2]),dtype=np.float64)
+	iRoi[segData==roiList[iIdx]] = 1.0
+
+	#Smooth i ROI for the first time
+	iSmooth = filt.gaussian_filter(iRoi,3.397).flatten()
+
+	#Get diagonal weight
+	wMatrix[iIdx,iIdx] = iSmooth.dot(iSmooth)
 
 	#Calculate t-values
 	for petIdx in range(nPet):
-		tMatrix[roiIdx,petIdx] = np.sum(xMat[:,roiIdx]*petData[:,petIdx])
+		tMatrix[iIdx,petIdx] = iSmooth.dot(petData[:,petIdx])
+
+	#Smooth i ROI again
+	iSmooth = filt.gaussian_filter(iSmooth,3.397)
+
+	for jIdx in tqdm(range(iIdx,nRoi)):
+
+		#Make j ROI
+		jRoi = np.zeros((seg.shape[0],seg.shape[1],seg.shape[2]),dtype=np.float64)
+		jRoi[segData==roiList[jIdx]] = 1.0
+
+		#Get weight
+		wMatrix[iIdx,jIdx] = iSmooth.dot(jRoi.flatten())
+		wMatrix[jIdx,iIdx] = wMatrix[iIdx,jIdx]
+
 
 #Save weight matrix
 nagini.writeText('%s_wMatrix.txt'%(args.out[0]),wMatrix)
 
-#Reshape PET back
-petData = petData.reshape((pet.shape[0],pet.shape[1],pet.shape[2],nPet))
+#Reshape pet data back
+petData = petData.reshape((seg.shape[0],seg.shape[1],seg.shape[2],nPet))
 
 #Loop through pet images
-roiCoef = np.zeros(nRoi)
-rbvData = np.zeros_like(petData)
+roiCoef = np.zeros(nRoi,dtype=np.float64)
+rbvData = np.zeros(petData.shape,dtype=np.float64)
 for petIdx in range(nPet):
 	
 	#Get regional coefficients
@@ -114,7 +121,7 @@ for petIdx in range(nPet):
 	nagini.writeText('%s_rsfAvg%03i.txt'%(args.out[0],petIdx),roiCoef)
 
 	#Make rsf image
-	rsfData = np.zeros_like(roiData)
+	rsfData = np.zeros((seg.shape[0],seg.shape[1],seg.shape[2]),dtype=np.float64)
 	for roiIdx in range(nRoi):
 		rsfData[segData==roiList[roiIdx]] = roiCoef[roiIdx]
 
